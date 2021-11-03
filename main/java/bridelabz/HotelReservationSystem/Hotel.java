90f260c71f46f5e19a2134f105c58a5aab956917
MallepoguAlankritha <mallepogualankritha@gmail.com>
2021-11-03 18:22:51 +0530

    Added:Ability to add hotel

--- main/java/bridelabz/HotelReservationSystem/Hotel.java
+++ main/java/bridelabz/HotelReservationSystem/Hotel.java
@@ -1,10 +1,27 @@
 package bridelabz.HotelReservationSystem;
-
 public class Hotel {
+    private String hotelName;
+    private int hotelRegularRate;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public String getHotelName() {
+        return hotelName;
+    }
 
-	}
+    public void setHotelName(String hotelName) {
+        this.hotelName = hotelName;
+    }
 
+    public int getHotelRegularRate() {
+        return hotelRegularRate;
+    }
+
+    public void setHotelRegularRate(int hotelRegularRate) {
+        this.hotelRegularRate = hotelRegularRate;
+    }
+
+    public Hotel(String hotelName, int hotelRegularRate) {
+        this.hotelName = hotelName;
+        this.hotelRegularRate = hotelRegularRate;
+
+    }
 }
--- test/java/bridelabz/HotelReservationSystem/HotelReservationTest.java
+++ test/java/bridelabz/HotelReservationSystem/HotelReservationTest.java
@@ -1,10 +1,21 @@
 package bridelabz.HotelReservationSystem;
+	import org.junit.Assert;
+	import org.junit.Test;
 
-public class HotelReservationTest {
+	public class HotelReservationTest {
+	    HotelReservationSystem hotelReservation = new HotelReservationSystem();
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	    @Test
+	    public void whenAddedHotel_ShouldReturn_AddedHotel() {
+	        try{
+	            boolean isValid1 = hotelReservation.addHotel( new Hotel("LakeWood",110));
+	            Assert.assertTrue(isValid1);
+	            boolean isValid2 = hotelReservation.addHotel( new Hotel("Bridgewood",160));
+	            Assert.assertTrue(isValid2);
+	            boolean isValid3 = hotelReservation.addHotel( new Hotel("Ridgewood",220));
+	            Assert.assertTrue(isValid3);
+	        }catch (Exception e){
+	            e.printStackTrace();
+	        }
+	    }
 	}
-
-}
